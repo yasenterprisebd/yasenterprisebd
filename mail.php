41a7f52b7340804298e5308147034e33cc60e62f
yasenterprisebd <143359063+yasenterprisebd@users.noreply.github.com>
2023-09-04 06:42:53 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,13 +3,14 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
+$subject= $_POST['Subject'];
 $message= $_POST['message'];
 
 
 $to = "info@yasenterprisebd.com";
 
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\n Subject = ". $subject . "\r\n Message =" . $message;
 
 $headers = "From: noreply@yasenterprisebd.com" . "\r\n" .
 "CC: somebodyelse@example.com";
