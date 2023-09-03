b06dcb3d1e9252333141ed1f3361709020a75799
yasenterprisebd <143359063+yasenterprisebd@users.noreply.github.com>
2023-09-04 05:47:13 +0800

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+
+$to = "info@yasenterprisebd.com";
+
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: noreply@yasenterprisebd.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
